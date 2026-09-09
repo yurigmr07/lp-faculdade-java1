c212426b9a2f771aae09f71fe24035b10068f553
yurigmr <yuricontaprof@gmail.com>
2026-09-08 22:19:47 -0300

    .

---
+++ src/main/java/beecrowd/Exe1001.java
@@ -0,0 +1,16 @@
+package beecrowd;
+
+import java.util.Scanner;
+
+public class Exe1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = Integer.parseInt(sc.nextLine());
+        int n2 = Integer.parseInt(sc.nextLine());
+
+
+        System.out.println("X= " + n1+n2);
+    }
+
+
+}
